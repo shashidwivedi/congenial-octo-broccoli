4645e6e5db6a79ca572bdb29fe32f72decd00d89
Shashi Dwivedi <srdwivedi16@gmail.com>
2024-02-28 01:11:19 +0530

    custom hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,7 +8,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -19,7 +20,6 @@ const SearchParams = () => {
     );
     const json = await res.json();
 
-    console.log(json);
     setPets(json.pets);
   }
 
