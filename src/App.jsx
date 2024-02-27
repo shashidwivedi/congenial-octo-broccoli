ce78d5e413f3a055392e8321d27b50500876ae0c
Shashi Dwivedi <srdwivedi16@gmail.com>
2024-02-27 21:47:14 +0530

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
-      <Pet name="Doink" animal="Cat" breed="Mix"></Pet>
+      <SearchParams />
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");
