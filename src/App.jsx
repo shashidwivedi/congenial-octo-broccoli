2e116f9826c17534942418737697166431b225be
Shashi Dwivedi <srdwivedi16@gmail.com>
2024-03-08 12:55:05 +0530

    server side rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, lazy, Suspense } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
 
@@ -20,31 +19,27 @@ const App = () => {
   const adoptedPetHook = useState(null);
 
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Suspense
-          fallback={
-            <div className="loading-pane">
-              <h2 className="loader">🐶</h2>
-            </div>
-          }
-        >
-          <AdoptedPetContext.Provider value={adoptedPetHook}>
-            <header>
-              <Link to="/">Adopt Me!</Link>
-            </header>
+    <QueryClientProvider client={queryClient}>
+      <Suspense
+        fallback={
+          <div className="loading-pane">
+            <h2 className="loader">🐶</h2>
+          </div>
+        }
+      >
+        <AdoptedPetContext.Provider value={adoptedPetHook}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
 
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
-          </AdoptedPetContext.Provider>
-        </Suspense>
-      </QueryClientProvider>
-    </BrowserRouter>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
+      </Suspense>
+    </QueryClientProvider>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
