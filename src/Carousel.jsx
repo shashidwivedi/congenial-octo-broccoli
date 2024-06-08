a237b9c8a155ef30958984bba08485bfa8fbddfa
Shashi Ranjan Dwivedi <shashi.dwivedi@MACPRO501.local>
2024-06-08 14:32:51 +0530

    add test for Carousel component

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -22,13 +22,14 @@ class Carousel extends Component {
 
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal hero" />
+        <img data-testid="hero" src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
             // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
             <img
               key={photo}
               src={photo}
+              data-testid={`thumbnail${index}`}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
               onClick={this.handleClick}
